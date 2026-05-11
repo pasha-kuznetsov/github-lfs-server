28d9bd72668113beadfa872171ca63c182a604e3
Pasha Kuznetsov <pasha@hyron.io>
2026-05-10 18:52:58 -0700

    cleanup

--- sql/locks.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS locks (
-  id         TEXT PRIMARY KEY NOT NULL,
-  owner      TEXT NOT NULL,
-  path       TEXT NOT NULL,
-  repo       TEXT NOT NULL,
-  locked_at  TEXT NOT NULL,
-  UNIQUE (repo, path)
-);
